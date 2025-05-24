828f3f475c902760eaf040e17ddbb5bd1b9ce6c5
Dat Nguyen <nguyendat9453@gmail.com>
2025-05-24 13:34:41 -0400

    Add lout functionality

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 
 
 # Get an instance of a logger
@@ -21,6 +21,7 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
+
 
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -39,8 +40,10 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request) # Terminate user session
+    data = {"userName":""} # Return empty username
+    return JsonResponse(data)
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
