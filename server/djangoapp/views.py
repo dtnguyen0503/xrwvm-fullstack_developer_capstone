6e5ed5f50098d78465c212d502759c013a6f1173
Dat Nguyen <nguyendat9453@gmail.com>
2025-05-25 01:30:53 -0400

    add loout configuration

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -4,7 +4,7 @@
 # from django.http import HttpResponseRedirect, HttpResponse
 # from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
+from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
 
@@ -41,7 +41,7 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    logout(request) # Terminate user session
+    logout(request) # Terminate user session    
     data = {"userName":""} # Return empty username
     return JsonResponse(data)
 
