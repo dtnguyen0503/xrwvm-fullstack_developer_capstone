bc151eca017119e4bdd75b304c2c2ad1b089d532
Dat Nguyen <nguyendat9453@gmail.com>
2025-05-31 23:32:10 -0400

    update get_dealers

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,8 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
+from .restapis import get_request, analyze_review_sentiments, post_review
+
 
 
 # Get an instance of a logger
@@ -89,7 +91,7 @@ def get_cars(request):
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
     
-def get_dealerships(request):
+def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
