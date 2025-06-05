882337f6b86d0a33c9b1d26bd95966d80a97dfb0
Dat Nguyen <nguyendat9453@gmail.com>
2025-06-05 12:43:58 -0400

    Add .flake8 config to ignore migrations and long lines

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse#, HttpResponseRedirect
+#from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
